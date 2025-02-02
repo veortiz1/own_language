7524e7819a0cbe43b40d91c2417a3297cbcd5821
veortiz1 <veortiz1@asu.edu>
2025-02-02 08:44:21 -0700

    added delete column

--- get_input.cpp
+++ get_input.cpp
@@ -6,39 +6,67 @@
 #include <string>
 #include <fstream>
 #include "file_operations.h"
-
 using namespace std;
 
-class MyFile {
+class MyFile
+{
 public:
     string delimiter;
     vector<string> file_lines;
     int delete_index;
     string delete_name;
+
+    void set_delete_index(string column_name)
+    {
+        string header = file_lines[0];
+        istringstream ss(header);
+        string token;
+        int counter = 0;
+        while (getline(ss, token, ','))
+        {
+            if (token == column_name)
+            {
+                break;
+            }
+            counter = counter + 1;
+        }
+        delete_index = counter;
+    }
+    void delete_column()
+    {
+
+        for (int i = 0; file_lines.size(); i++)
+        {
+        }
+    }
 };
 
-string manage_input(string input) {
-   
+string manage_input(string input)
+{
 
     MyFile my_csv;
-      my_csv.file_lines=create_file();
+    my_csv.file_lines = create_file();
+    my_csv.delimiter = ",";
 
     vector<string> tokens;
     istringstream ss(input);
     string token;
 
-    while (ss >> token) {
+    while (ss >> token)
+    {
         tokens.push_back(token);
     }
 
     string method = to_uppercase(tokens[0]);
-     
 
-    if (method == "DELETE") {
-        string row_or_column=to_uppercase(tokens[1]);
-      if(row_or_column=="COLUMN"){
-
-      }
+    if (method == "DELETE")
+    {
+        string row_or_column = to_uppercase(tokens[1]);
+        if (row_or_column == "COLUMN")
+        {
+            my_csv.set_delete_index(tokens[2]);
+            cout << to_string(my_csv.delete_index);
+        }
     }
 
     return "yues";
--- run_file.cpp
+++ run_file.cpp
@@ -9,6 +9,9 @@ int main(){
 
 
     manage_input("delete column Title");
+       manage_input("delete column Author");
+          manage_input("delete column Year");
+             manage_input("delete column Genre");
 
 
 

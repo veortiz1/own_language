a69b5c77b12d7c0d789f8bf6070b81e14b54aea3
veortiz1 <veortiz1@asu.edu>
2025-02-02 08:03:30 -0700

    add file operations

--- get_input.cpp
+++ get_input.cpp
@@ -7,59 +7,16 @@
 #include <fstream>
 
 
-
-void delete_function(std::vector <std::string> tokens){
-    std::cout<<"in delete function!";
-    std::string token_1=to_uppercase(tokens[1]);
-    std::string token_2=tokens[2];
-
-    
-
-    if(token_1=="COLUMN"){
-        std::cout<<"column!!!!";
-
-        std::ifstream  data("file.csv");
-         std::string line;
-         int row=0;
-         int colum_number;
-         std::vector<std::vector<std::string>> new_csv;
-    while(std::getline(data,line))
-    {
-        std::stringstream  lineStream(line);
-        std::string        cell;
-         int counter=0;
-         std::vector <std::string> updated_row;
-        while(std::getline(lineStream,cell,','))
-        {
-           
-         if(row==0){
-            if(cell==tokens[2]){
-              colum_number=counter;
-              break;
-            }
-         }
-         else{
-            if (counter==colum_number){
-                std::cout<<cell;
-            }
-            else{
-             updated_row.push_back(cell);
-            }
-         }
-         counter=counter+1;
-        }
-        row=row+1;
-    }
-        
-    }
-    else{
-        std::cout<<"nahhhj";
-    }
+class edit_file{
+    public:
+    std::string delimiter;
     
 
 
+};
 
-}
+
+
 
 
 std::string manage_input(std::string input){
